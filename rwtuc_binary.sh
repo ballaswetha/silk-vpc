a2a6bb5e26087e883649b6f3c7042e7d388a959a
sudheer chamarthi <sudheer.chamarthi@gmail.com>
2020-08-11 17:28:20 +0000

    Update rwtuc_binary.sh

--- rwtuc_binary.sh
+++ rwtuc_binary.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
-# This is to convert files to silk binray format
-source .env
 CURRENT_HOUR=`date +%H`
-# Looks for files that are not updated in last 15 minutes and convert them
-for file in `find ${LOCAL_DIR} -type f -mmin +15`
+for file in `find ${LOCAL_DIR} -type f -mmin +15 ! -name '*.log.gz'`
 do
-if [ "${file: -2}" != ${CURRENT_HOUR} ] #Ignore if the file exteniosn equals to current hour
+if [ "${file: -2}" != ${CURRENT_HOUR} ]
 then
-	OUT_FILE_NAME='{}'
-	echo $OUT_FILE_NAME
-	if `rwtuc ${file} --output-path=${RWSENDER_INCOMING_DIRECTORY}/${file##*/}`
-	then
-	echo "Delete file"
-	#rm  ${file} #To DO. Remove the comment and dlete file once it is processed to binary
-fi
+rwtuc ${file} --output-path=${RWSENDER_INCOMING_DIRECTORY}/${file##*/} && rm  -v ${file}
 fi
 done
