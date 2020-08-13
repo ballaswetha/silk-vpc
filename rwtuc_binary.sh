7267d244ca8f9d09b31eee07f2b6934157dd6b21
root <root@ip-172-31-66-187.ec2.internal>
2020-08-13 09:18:10 +0000

    Mounted silk data volume

--- rwtuc_binary.sh
+++ rwtuc_binary.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 CURRENT_HOUR=`date +%H`
+TODAY_DATE=`date +"%Y/%m/%d"`
+mkdir -p ${RWSENDER_INCOMING_DIRECTORY}/${TODAY_DATE}
 for file in `find ${LOCAL_DIR} -type f -mmin +15 ! -name '*.log.gz'`
 do
 if [ "${file: -2}" != ${CURRENT_HOUR} ]
 then
-rwtuc ${file} --output-path=${RWSENDER_INCOMING_DIRECTORY}/${file##*/} && rm  -v ${file}
+rwtuc ${file} --output-path=${RWSENDER_INCOMING_DIRECTORY}/${TODAY_DATE}/${file##*/} && rm  -v ${file}
 fi
 done
